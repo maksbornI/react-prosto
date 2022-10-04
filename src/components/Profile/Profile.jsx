ff5d50930af72a53ae60d6d3a3aec3cce5e23852
maksbornI <maksborn@gmail.com>
2022-10-04 21:21:54 +0200

    Profile class

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,9 +3,10 @@ import MyPosts from './MyPosts/MyPosts';
 import s from './Profile.module.css';
 
 const Profile = () => {
-  return <div>
+  return <div className={s.profile}>
+
     <div>
-      <img src='https://www.newelectronics.co.uk/article-images/233252/future_popup.jpg' />
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7XC4AYqnDlg9bYp1aPdNQjRQTZwYjp2AICA&usqp=CAU' />
     </div>
     <div>
       Avatar
